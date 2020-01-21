a1e728c7379eca650089ab7d734a3412852c7fba
Chloé <rightfold+ck@gmail.com>
2020-01-21 21:06:09 +0100

    sitrep: Add PostgreSQL DBMS configuration

--- default.nix
+++ default.nix
@@ -14,6 +14,7 @@ in
         pkgs.gcc
         pkgs.hivemind
         pkgs.ldc
+        pkgs.postgresql_12
         pkgs.shellcheck
         pkgs.socat
     ]
--- sitrep/build.pl
+++ sitrep/build.pl
@@ -12,7 +12,7 @@ use File::Which qw(which);
 # Constants
 
 my %path = map { $_ => which($_) // die("which: $_") }
-               qw(bash hivemind socat);
+               qw(bash hivemind initdb postgres socat);
 
 my @ldFlags          = split(/\s+/, qx{pkg-config --libs libsodium});
 my @ldcFlags         = (qw(-O3 -dip1000), map("-L$_", @ldFlags));
@@ -32,6 +32,7 @@ my @dLibrarySourceFiles = qw(
 );
 
 my $buildDir = 'build/sitrep';
+my $stateDir = 'state/sitrep';
 
 my $port = 1080;
 
@@ -48,17 +49,54 @@ system('ldc2', @ldcFlags,
        'sitrep/sitrep-receive.d', @dLibrarySourceFiles,
        "-of=$buildDir/sitrep-receive");
 
+write_file("$buildDir/postgresql.conf", <<EOF);
+# Paths to files used by the DBMS.
+data_directory=$stateDir/postgresql-data
+hba_file=$buildDir/postgresql.hba
+ident_file=$buildDir/postgresql.ident
+
+# Normalize settings for client connections.
+DateStyle=ISO
+IntervalStyle=sql_standard
+TimeZone=UTC
+client_encoding=UTF8
+EOF
+
+write_file("$buildDir/postgresql.hba", <<EOF);
+#       database   user       auth-method
+local   all        postgres   md5
+EOF
+
+write_file("$buildDir/postgresql.ident", <<EOF);
+EOF
+
 write_file("$buildDir/Procfile", <<EOF);
 sitrep-receive: $path{socat} -d TCP-LISTEN:$port,fork,reuseaddr EXEC:$buildDir/sitrep-receive
+postgresql: $path{postgres} --config-file=$buildDir/postgresql.conf -k \$PWD/$stateDir/postgresql-sockets
 EOF
 
 write_file("$buildDir/hivemind", { perms => 0755 }, <<EOF);
 #!$path{bash}
 set -euo pipefail
+
 if (( \$# != 0 )); then
     1>&2 echo "\$0: Too many arguments"
     exit 1
 fi
+
+if ! [[ -e $stateDir/postgresql-data ]]; then
+    $path{initdb}                           \\
+        --pgdata=$stateDir/postgresql-data  \\
+        --username=postgres                 \\
+        --pwfile=<(echo postgres)           \\
+        --locale=en_US.UTF-8
+    rm $stateDir/postgresql-data/{pg_hba.conf,pg_ident.conf,postgresql.auto.conf,postgresql.conf}
+fi
+
+if ! [[ -e $stateDir/postgresql-sockets ]]; then
+    mkdir $stateDir/postgresql-sockets
+fi
+
 exec $path{hivemind} --root "\$PWD" $buildDir/Procfile
 EOF
 system('shellcheck', "$buildDir/hivemind");
